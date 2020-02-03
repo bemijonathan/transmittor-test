14c59eb17f9cb0001c9829784eb57db1364e6914
bemijonathan <bemijonathan@gmail.com>
2020-02-03 08:52:23 +0100

    pushing

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -9,60 +9,198 @@ export default function Table() {
     <>
       <table className="text-left mt-4 w-full" style={{ borderCollapse: "collapse" }}>
         <thead>
-          <tr>
-            <th style={{background:"#EAEEF0"}} className="py-4 px-6 rounded font-sans font-medium text-sm text-grey border-b border-grey-light">Item typr</th>
-            <th style={{background:"#EAEEF0"}} className="py-4 px-6 rounded font-sans font-medium text-sm text-grey border-b border-grey-light">Transaction Number</th>
-            <th style={{background:"#EAEEF0"}} className="py-4 px-6 rounded font-sans font-medium text-sm text-grey border-b border-grey-light">Price</th>
-            <th style={{background:"#EAEEF0"}} className="py-4 px-6 rounded font-sans font-medium text-sm text-grey border-b border-grey-light">Date</th>
-            <th style={{background:"#EAEEF0"}} className="py-4 px-6 rounded font-sans font-medium text-sm text-grey border-b border-grey-light">Status</th>
+          <tr style={{color:"#7F8FA4"}}>
+            <th style={{background:"#EAEEF0"}} className="py-4 px-6 rounded text-center font-sans font-medium text-sm border-b border-grey-light">Item typr</th>
+            <th style={{background:"#EAEEF0"}} className="py-4 px-6 rounded text-center font-sans font-medium text-sm border-b border-grey-light">Price</th>
+            <th style={{background:"#EAEEF0"}} className="py-4 px-6 rounded text-center font-sans font-medium text-sm border-b border-grey-light">Transaction Number</th>
+            <th style={{background:"#EAEEF0"}} className="py-4 px-6 rounded text-center font-sans font-medium text-sm border-b border-grey-light">Date</th>
+            <th style={{background:"#EAEEF0"}} className="py-4 px-6 rounded text-center font-sans font-medium text-sm border-b border-grey-light">Status</th>
 
           </tr>
         </thead>
         <tbody>
           <tr className="bg-white">
-            <td className="py-4 px-6 border-b border-grey-light">Conversations</td>
-            <td className="py-4 px-6 border-b border-grey-light text-center">❌</td>
-            <td className="py-4 px-6 border-b border-grey-light">Conversations</td>
-            <td className="py-4 px-6 border-b border-grey-light text-center">❌</td>
-            <td className="py-4 px-6 border-b border-grey-light text-center">❌</td>
+            <td className="py-4 px-6 border-b border-grey-light flex items-center">
+              <div style={{width:"36px", height:"36px", borderRadius:"18px", fontSize:"12px"}} className=" text-white bg-gray-500 flex items-center justify-center">
+                VW 
+              </div> <span className="ml-5">Apple Macbook 15" 250 SSD 12GB</span>
+            </td>
+            <td className="py-4 px-6 border-b border-grey-light text-center">$7800</td>
+            <td className="py-4 px-6 border-b border-grey-light text-center">1234567890</td>
+            <td className="py-4 px-6 border-b border-grey-light text-center">12:30</td>
+            <td className="py-4 px-6 border-b border-grey-light text-center flex"> 
+              <button className="border border-yellow-600 rounded-lg flex items-center mr-2 justify-center" style={{width: '120px',fontSize:"13px",height: '33px', borderRadius:'30px'}}> 
+               <div className="ball mr-3">
+              </div>  Pending
+              </button>
+              <button className="p-2 text-lg ml-10" style={{lineHeight:1,}}>
+                  ⌄
+              </button>
+            </td>
           </tr>
-          <tr className="bg-white">
-            <td className="py-4 px-6 border-b border-grey-light">Question-Buttons</td>
-            <td className="py-4 px-6 border-b border-grey-light text-center">❌</td>
-            <td className="py-4 px-6 border-b border-grey-light">Conversations</td>
-            <td className="py-4 px-6 border-b border-grey-light text-center">❌</td>
-            <td className="py-4 px-6 border-b border-grey-light text-center">❌</td>
-          </tr>
-          <tr className="bg-white">
-            <td className="py-4 px-6 border-b border-grey-light">Image Attachment</td>
-            <td className="py-4 px-6 border-b border-grey-light text-center">✅ </td>
-            <td className="py-4 px-6 border-b border-grey-light">Conversations</td>
-            <td className="py-4 px-6 border-b border-grey-light text-center">❌</td>
-            <td className="py-4 px-6 border-b border-grey-light text-center">❌</td>
 
-          </tr>
           <tr className="bg-white">
-            <td className="py-4 px-6 border-b border-grey-light">Video Attachment</td>
-            <td className="py-4 px-6 border-b border-grey-light text-center">❌</td>
-            <td className="py-4 px-6 border-b border-grey-light text-center">❌</td>
-            <td className="py-4 px-6 border-b border-grey-light text-center">❌</td>
-            <td className="py-4 px-6 border-b border-grey-light text-center">❌</td>
+            <td className="py-4 px-6 border-b border-grey-light flex items-center">
+              <div style={{width:"36px", height:"36px", borderRadius:"18px", fontSize:"12px"}} className=" text-white bg-gray-500 flex items-center justify-center">
+                VW 
+              </div> <span className="ml-5">Apple Macbook 15" 250 SSD 12GB</span>
+            </td>
+            <td className="py-4 px-6 border-b border-grey-light text-center">$7800</td>
+            <td className="py-4 px-6 border-b border-grey-light text-center">1234567890</td>
+            <td className="py-4 px-6 border-b border-grey-light text-center">12:30</td>
+            <td className="py-4 px-6 border-b border-grey-light text-center flex"> 
+              <button className="border border-yellow-600 rounded-lg flex items-center mr-2 justify-center" style={{width: '120px',fontSize:"13px",height: '33px', borderRadius:'30px'}}> 
+               <div className="ball mr-3">
+              </div>  Pending
+              </button>
+              <button className="p-2 text-lg ml-10" style={{lineHeight:1,}}>
+                  ⌄
+              </button>
+            </td>
           </tr>
-          <tr className="bg-white">
-            <td className="py-4 px-6 border-b border-grey-light">Audio Attachment</td>
-            <td className="py-4 px-6 border-b border-grey-light text-center">❌</td>
-            <td className="py-4 px-6 border-b border-grey-light">Conversations</td>
-            <td className="py-4 px-6 border-b border-grey-light text-center">❌</td>
-            <td className="py-4 px-6 border-b border-grey-light text-center">❌</td>
-          </tr>
-          <tr className="bg-white">
-            <td className="py-4 px-6 border-b border-grey-light">Location Attachment</td>
-            <td className="py-4 px-6 border-b border-grey-light text-center">❌</td>
-            <td className="py-4 px-6 border-b border-grey-light">Conversations</td>
-            <td className="py-4 px-6 border-b border-grey-light text-center">❌</td>
-            <td className="py-4 px-6 border-b border-grey-light text-center">❌</td>
 
+          <tr className="bg-white">
+            <td className="py-4 px-6 border-b border-grey-light flex items-center">
+              <div style={{width:"36px", height:"36px", borderRadius:"18px", fontSize:"12px"}} className=" text-white bg-gray-500 flex items-center justify-center">
+                VW 
+              </div> <span className="ml-5">Apple Macbook 15" 250 SSD 12GB</span>
+            </td>
+            <td className="py-4 px-6 border-b border-grey-light text-center">$7800</td>
+            <td className="py-4 px-6 border-b border-grey-light text-center">1234567890</td>
+            <td className="py-4 px-6 border-b border-grey-light text-center">12:30</td>
+            <td className="py-4 px-6 border-b border-grey-light text-center flex"> 
+              <button className="border border-yellow-600 rounded-lg flex items-center mr-2 justify-center" style={{width: '120px',fontSize:"13px",height: '33px', borderRadius:'30px'}}> 
+               <div className="ball mr-3">
+              </div>  Pending
+              </button>
+              <button className="p-2 text-lg ml-10" style={{lineHeight:1,}}>
+                  ⌄
+              </button>
+            </td>
           </tr>
+
+          <tr className="bg-white">
+            <td className="py-4 px-6 border-b border-grey-light flex items-center">
+              <div style={{width:"36px", height:"36px", borderRadius:"18px", fontSize:"12px"}} className=" text-white bg-gray-500 flex items-center justify-center">
+                VW 
+              </div> <span className="ml-5">Apple Macbook 15" 250 SSD 12GB</span>
+            </td>
+            <td className="py-4 px-6 border-b border-grey-light text-center">$7800</td>
+            <td className="py-4 px-6 border-b border-grey-light text-center">1234567890</td>
+            <td className="py-4 px-6 border-b border-grey-light text-center">12:30</td>
+            <td className="py-4 px-6 border-b border-grey-light text-center flex"> 
+              <button className="border border-yellow-600 rounded-lg flex items-center mr-2 justify-center" style={{width: '120px',fontSize:"13px",height: '33px', borderRadius:'30px'}}> 
+               <div className="ball mr-3">
+              </div>  Pending
+              </button>
+              <button className="p-2 text-lg ml-10" style={{lineHeight:1,}}>
+                  ⌄
+              </button>
+            </td>
+          </tr>
+
+
+          <tr className="bg-white">
+            <td className="py-4 px-6 border-b border-grey-light flex items-center">
+              <div style={{width:"36px", height:"36px", borderRadius:"18px", fontSize:"12px"}} className=" text-white bg-gray-500 flex items-center justify-center">
+                VW 
+              </div> <span className="ml-5">Apple Macbook 15" 250 SSD 12GB</span>
+            </td>
+            <td className="py-4 px-6 border-b border-grey-light text-center">$7800</td>
+            <td className="py-4 px-6 border-b border-grey-light text-center">1234567890</td>
+            <td className="py-4 px-6 border-b border-grey-light text-center">12:30</td>
+            <td className="py-4 px-6 border-b border-grey-light text-center flex"> 
+              <button className="border border-yellow-600 rounded-lg flex items-center mr-2 justify-center" style={{width: '120px',fontSize:"13px",height: '33px', borderRadius:'30px'}}> 
+               <div className="ball mr-3">
+              </div>  Pending
+              </button>
+              <button className="p-2 text-lg ml-10" style={{lineHeight:1,}}>
+                  ⌄
+              </button>
+            </td>
+          </tr>
+
+          <tr className="bg-white">
+            <td className="py-4 px-6 border-b border-grey-light flex items-center">
+              <div style={{width:"36px", height:"36px", borderRadius:"18px", fontSize:"12px"}} className=" text-white bg-gray-500 flex items-center justify-center">
+                VW 
+              </div> <span className="ml-5">Apple Macbook 15" 250 SSD 12GB</span>
+            </td>
+            <td className="py-4 px-6 border-b border-grey-light text-center">$7800</td>
+            <td className="py-4 px-6 border-b border-grey-light text-center">1234567890</td>
+            <td className="py-4 px-6 border-b border-grey-light text-center">12:30</td>
+            <td className="py-4 px-6 border-b border-grey-light text-center flex"> 
+              <button className="border border-yellow-600 rounded-lg flex items-center mr-2 justify-center" style={{width: '120px',fontSize:"13px",height: '33px', borderRadius:'30px'}}> 
+               <div className="ball mr-3">
+              </div>  Pending
+              </button>
+              <button className="p-2 text-lg ml-10" style={{lineHeight:1,}}>
+                  ⌄
+              </button>
+            </td>
+          </tr>
+
+
+          <tr className="bg-white">
+            <td className="py-4 px-6 border-b border-grey-light flex items-center">
+              <div style={{width:"36px", height:"36px", borderRadius:"18px", fontSize:"12px"}} className=" text-white bg-gray-500 flex items-center justify-center">
+                VW 
+              </div> <span className="ml-5">Apple Macbook 15" 250 SSD 12GB</span>
+            </td>
+            <td className="py-4 px-6 border-b border-grey-light text-center">$7800</td>
+            <td className="py-4 px-6 border-b border-grey-light text-center">1234567890</td>
+            <td className="py-4 px-6 border-b border-grey-light text-center">12:30</td>
+            <td className="py-4 px-6 border-b border-grey-light text-center flex"> 
+              <button className="border border-yellow-600 rounded-lg flex items-center mr-2 justify-center" style={{width: '120px',fontSize:"13px",height: '33px', borderRadius:'30px'}}> 
+               <div className="ball mr-3">
+              </div>  Pending
+              </button>
+              <button className="p-2 text-lg ml-10" style={{lineHeight:1,}}>
+                  ⌄
+              </button>
+            </td>
+          </tr>
+
+          <tr className="bg-white">
+            <td className="py-4 px-6 border-b border-grey-light flex items-center">
+              <div style={{width:"36px", height:"36px", borderRadius:"18px", fontSize:"12px"}} className=" text-white bg-gray-500 flex items-center justify-center">
+                VW 
+              </div> <span className="ml-5">Apple Macbook 15" 250 SSD 12GB</span>
+            </td>
+            <td className="py-4 px-6 border-b border-grey-light text-center">$7800</td>
+            <td className="py-4 px-6 border-b border-grey-light text-center">1234567890</td>
+            <td className="py-4 px-6 border-b border-grey-light text-center">12:30</td>
+            <td className="py-4 px-6 border-b border-grey-light text-center flex"> 
+              <button className="border border-yellow-600 rounded-lg flex items-center mr-2 justify-center" style={{width: '120px',fontSize:"13px",height: '33px', borderRadius:'30px'}}> 
+               <div className="ball mr-3">
+              </div>  Pending
+              </button>
+              <button className="p-2 text-lg ml-10" style={{lineHeight:1,}}>
+                  ⌄
+              </button>
+            </td>
+          </tr>
+
+          <tr className="bg-white">
+            <td className="py-4 px-6 border-b border-grey-light flex items-center">
+              <div style={{width:"36px", height:"36px", borderRadius:"18px", fontSize:"12px"}} className=" text-white bg-gray-500 flex items-center justify-center">
+                VW 
+              </div> <span className="ml-5">Apple Macbook 15" 250 SSD 12GB</span>
+            </td>
+            <td className="py-4 px-6 border-b border-grey-light text-center">$7800</td>
+            <td className="py-4 px-6 border-b border-grey-light text-center">1234567890</td>
+            <td className="py-4 px-6 border-b border-grey-light text-center">12:30</td>
+            <td className="py-4 px-6 border-b border-grey-light text-center flex"> 
+              <button className="border border-yellow-600 rounded-lg flex items-center mr-2 justify-center" style={{width: '120px',fontSize:"13px",height: '33px', borderRadius:'30px'}}> 
+               <div className="ball mr-3">
+              </div>  Pending
+              </button>
+              <button className="p-2 text-lg ml-10" style={{lineHeight:1,}}>
+                  ⌄
+              </button>
+            </td>
+          </tr>
+          
         </tbody>
       </table>
 
